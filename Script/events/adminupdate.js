f2067fcf4fe907a90f39f9c1ae9dbefd4a653515
Somrat8410 <somrat8410@gmail.com>
2025-08-29 02:04:25 +0600

    Update adminupdate.js

--- Script/events/adminupdate.js
+++ Script/events/adminupdate.js
@@ -36,7 +36,7 @@ try {
             else if (logMessageData.ADMIN_EVENT == "remove_admin") {  
                 dataThread.adminIDs = dataThread.adminIDs.filter(item => item.id != logMessageData.TARGET_ID);  
                 if (global.configModule[this.config.name].sendNoti) api.sendMessage(`»» 𝙽𝙾𝚃𝙸𝙲𝙴 𝚄𝙿𝙳𝙰𝚃𝙴 «« 
-                𝚞𝚜𝚎𝚛${logMessageData.TARGET_ID}বেশি চুল পাকনামি করার কারণে🥴 তোকে এডমিন থেকে\n  লাথি মেরে  বের করে দেওয়া হল 😀😂`, threadID, async (error, info) => {  
+                𝚞𝚜𝚎𝚛${logMessageData.TARGET_ID}বেশি চুল পাকনামি করার কারণে তোকে এডমিন থেকে\n লাথি মেরে বের করে দেওয়া হল-!!😀😂`, threadID, async (error, info) => {  
                     if (global.configModule[this.config.name].autoUnsend) {  
                         await new Promise(resolve => setTimeout(resolve, global.configModule[this.config.name].timeToUnsend * 1000));  
                         return api.unsendMessage(info.messageID);  
