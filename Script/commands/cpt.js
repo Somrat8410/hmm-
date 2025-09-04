e8dfc26a6e696aaede1defe6c09afc3163032c64
Somrat8410 <somrat8410@gmail.com>
2025-09-05 01:47:36 +0600

    Update cpt.js

--- Script/commands/cpt.js
+++ Script/commands/cpt.js
@@ -1,79 +1,56 @@
 const fs = require("fs-extra");
 const request = require("request");
-const axios = require("axios");
 
 module.exports.config = {
-  name: "/",
+  name: "/", // এখন কমান্ড হবে "/"
   version: "1.0.0",
   hasPermssion: 0,
   credits: "SHAHADAT SCRIPT",
-  description: "Bot ব্যবহার করার নিয়ম শেখানোর জন্য",
-  commandCategory: "system",
+  description: "Slash কমান্ড দিলে ওয়েলকাম মেসেজ পাঠাবে",
+  commandCategory: "fun",
   usages: "/",
   cooldowns: 5,
-  dependencies: {
-    "request": "",
-    "fs-extra": "",
-    "axios": ""
-  }
 };
 
-module.exports.run = async ({ api, event }) => {
-  const prefix = global.config.PREFIX || "/";
-  const requestLib = global.nodemodule["request"];
-  const fsLib = global.nodemodule["fs-extra"];
+module.exports.run = async function ({ api, event }) {
+  const { threadID } = event;
 
-  // ওয়েলকাম মেসেজ টেমপ্লেট
-  var welcomeMessages = [
-    `🌸 Assalamu Alaikum, dear member!  
+  // মেসেজ
+  const msg = `🌸 Assalamu Alaikum, dear member!  
 ✨ আমার বট ব্যবহার করে অনেক মজা পাবেন।  
 📌 Here's how you can use the bot: 👇  
-\n💝 help ➤ View all commands  
+
+💝 help ➤ View all commands  
 💡 info ➤ About the bot  
 🤖 baby ➤ Automatic Chat  
 📷 random pic ➤ Random picture  
-\nPro Tip: Explore hidden features! 🌺`
-  ];
 
-  // র‍্যান্ডম মেসেজ সিলেক্ট
-  var msg = welcomeMessages[Math.floor(Math.random() * welcomeMessages.length)];
+Pro Tip: Explore hidden features! 🌺`;
 
-  // কিছু র‍্যান্ডম ইমেজ লিংক (imgur থেকে নেওয়া)
-  var images = [
+  // কিছু র‍্যান্ডম ইমেজ (imgur থেকে)
+  const images = [
     "https://i.imgur.com/vr59gnp.jpg",
     "https://i.imgur.com/3uW3OrW.jpg",
     "https://i.imgur.com/ENebaVC.jpg",
     "https://i.imgur.com/tl6Lt8Y.jpg",
     "https://i.imgur.com/nI6W6sM.jpg",
-    "https://i.imgur.com/ObLm7Df.jpg",
-    "https://i.imgur.com/ETDkFyf.jpg",
-    "https://i.imgur.com/niBV0ZV.jpg",
-    "https://i.imgur.com/fQZhwWg.jpg",
-    "https://i.imgur.com/gUQTUkN.jpg",
-    "https://i.imgur.com/xXQIZ7U.jpg",
-    "https://i.imgur.com/QIYO8lo.jpg",
-    "https://i.imgur.com/d9TRavK.jpg",
-    "https://i.imgur.com/X1OPkox.jpg",
-    "https://i.imgur.com/7wZ1Ael.jpg",
-    "https://i.imgur.com/2ProK0c.jpg",
-    "https://i.imgur.com/KxkUNYJ.jpg",
-    "https://i.imgur.com/yT294WF.jpg"
+    "https://i.imgur.com/ObLm7Df.jpg"
   ];
 
-  // র‍্যান্ডম ছবি বাছাই
-  var img = images[Math.floor(Math.random() * images.length)];
+  // র‍্যান্ডম ছবি সিলেক্ট করা
+  const img = images[Math.floor(Math.random() * images.length)];
 
-  // ছবি ডাউনলোড করে সেন্ড করা
-  requestLib(encodeURI(img))
-    .pipe(fsLib.createWriteStream(__dirname + "/welcome.jpg"))
+  // ছবি ডাউনলোড করে পাঠানো
+  request(encodeURI(img))
+    .pipe(fs.createWriteStream(__dirname + "/slash.jpg"))
     .on("close", () => {
       api.sendMessage(
         {
           body: msg,
-          attachment: fsLib.createReadStream(__dirname + "/welcome.jpg")
+          attachment: fs.createReadStream(__dirname + "/slash.jpg")
         },
-        event.threadID,
-        () => fsLib.unlinkSync(__dirname + "/welcome.jpg")
+        threadID,
+        () => fs.unlinkSync(__dirname + "/slash.jpg")
       );
     });
 };
